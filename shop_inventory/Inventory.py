ef7a1d1573bd3d0991dc4bbf08edfc2b8639c055
jormungandr1105 <maxtmarshall99@gmail.com>
2021-09-14 16:37:21 -0400

    working while my professor was teaching basic linux

--- shop_inventory/Inventory.py
+++ shop_inventory/Inventory.py
@@ -1,14 +1,14 @@
 import os
 import sys
 import json
-
-from Item import *
+import datetime
 
 
 class Inventory(): 
 
 	items = {}
 	locations = {}
+	families = {}
 
 	def __init__(self) -> None:
 		pass
@@ -18,28 +18,65 @@ class Inventory():
 		f = open(json_file, "r")
 		text = f.read()
 		f.close()
-		print(text)
 		data = json.loads(text)
-		print(data)
 		self.items = data["items"]
 		self.locations = data["locations"]
+		self.families = data["families"]
 
 	
 	def save_json(self, json_file):
-		self.items["m8_20"] = "L4"
-		self.locations["L4"] = "m8_20"
 		f = open(json_file, 'w+')
 		form_dict = {}
 		form_dict["items"] = self.items
 		form_dict["locations"] = self.locations
+		form_dict["families"] = self.families
 		form_text = json.dumps(form_dict, indent=4)
 		f.write(form_text)
 		f.close()
 
 
-	def search(self, text):
+	def search_item(self, name):
 		# Search database for parameter
-		pass
+		if name in self.items:
+			return self.items[name]
+		else:
+			return None
 
-	def move(self, item):
-		pass
+	def move(self, item, new_location):
+		name = item["name"]
+		location = item["location"]
+
+
+	def create_item(self, item_name, item_location = "", item_family = "", item_quantity = 1, quantity_required = 1, desc = ""):
+
+		if item_name in self.items:
+			self.items[item_name]["item_quantity"] += item_quantity
+		else:
+			new_item = {}
+			new_item["name"] = item_name
+			new_item["location"] = item_location
+			new_item["family"] = item_family
+			new_item["quantity"] = item_quantity
+			new_item["quantity_required"] = quantity_required
+			new_item["desc"] = desc
+			new_item["date_mod"] = str(datetime.datetime.utcnow())
+			self.items[item_name] = new_item
+
+			if item_location != "":
+				self.locations[item_location] = item_name
+
+			if item_family != "":
+				if item_family in self.families:
+					self.families[item_family].append(item_name)
+				else:
+					self.families[item_family] = [item_name]
+
+	def delete_item(self, item):
+		name = item["name"]
+		location = item["location"]
+		family = item["family"]
+
+		self.items.pop(name)
+		self.locations[location] = ""
+		self.families[family].remove(name)
+		
--- shop_inventory/main.py
+++ shop_inventory/main.py
@@ -1,14 +1,76 @@
-import time
+#!/usr/bin/python3
+import datetime
 import json
 from Inventory import *
 
+def create():
+	global inv
+	print("CREATING ITEM")
+	name = input("ENTER NAME: ")
+	location = input("ENTER LOCATION: ")
+	family = input("ENTER FAMILY: ")
+	quantity = int(input("ENTER QUANTITY: "))
+	quant_need = int(input("ENTER QUANTITY NEEDED: "))
+	desc = input("ENTER DESCRIPTION: ")
+	inv.create_item(name, location, family, quantity, quant_need, desc)
 
 
-locations = {}
+def delete():
+	global inv
+	print("DELETING ITEM")
+	name = input("ENTER NAME: ")
+	del_item = inv.search_item(name)
+	if del_item != None:
+		confirmation = input("CONFIRM DELETE? (y/N): ")
+		if confirmation.lower() == "y":
+			inv.delete_item(del_item)
+	else:
+		print("ITEM NOT FOUND")
 
+
+def use():
+	global inv
+	name = input("ENTER NAME: ")
+	item = inv.search_item(name)
+	if item != None:
+		current_amt = item["quantity"]
+		print("CURRENT QUANTITY: {0}".format(current_amt))
+	number = int(input("ENTER AMT USED: "))
+
+
+def process_command(cmd):
+	global inv
+	cmd_array = cmd.split(" ")
+	if len(cmd_array) == 1:
+		# Step by step
+		pass
+	else:
+		pass
+	return 0
+
+
+inv = Inventory()
 
 
 if __name__ == '__main__':
-	inv = Inventory()
 	inv.load_json("inventory.json")
+	cmd = ""
+	#'''
+	cont = True
+	while cont:
+		cmd = input("Enter Command: ")
+		# FILL THIS OUT BETTER LATER
+		if cmd == "create":
+			create()
+		elif cmd == "delete":
+			delete()
+		elif cmd == "use":
+			use()
+		elif cmd == "add":
+			pass
+		else:
+			cont = process_command(cmd)
+		
+	#'''
+	
 	inv.save_json("inventory.json")
